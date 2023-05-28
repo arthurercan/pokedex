757dca528832bfb989c7deca73fe3e4d99b04b38
arthurercan <arthurtaylan@hotmail.com>
2023-05-28 00:22:57 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { PokemonComponent } from './services/pokemon/pokemon.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent,
+    PokemonCardComponent,
+    PokemonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
